3be1fb0eb9f27648c7971025ae80a5db1b8daf3c
Jason Wisniewski <87104768+JasonWisniewski@users.noreply.github.com>
2022-07-29 11:27:42 -0600

    user model name and password

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
 
+      t.timestamps
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
