f219a8b5a750606f29cb1703c394409da8cd2e7f
Jason Wisniewski <87104768+JasonWisniewski@users.noreply.github.com>
2022-08-01 10:48:02 -0600

    can add user in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_secure_password
+
+  # email : string
+  # password_digest: string
+  # password: string virtual
+  # password_confirmation: string virtual
+  #
+  # has_secure_password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.first_or_create(
+  email:"jason@email.com",
+  password_confirmation: 'password'
+)
