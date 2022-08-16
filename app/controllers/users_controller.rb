8a079e9fdc0bb0c20c7860915b9ed49ab28ee37f
Jason Wisniewski <87104768+JasonWisniewski@users.noreply.github.com>
2022-08-16 10:03:37 -0600

    start of testing w/ rspec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  has_secure_password
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  # has_secure_password
+  # validates_presence_of :email
+  # validates_uniqueness_of :email
 
   #GET users
   def index
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  before(:all) do
+    adsf
+  end
+
+end
